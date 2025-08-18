79b6e6b421b8b53da0187ef95eecbf9dc1cf4a54
K-Zuki <85270181+K-Zuki@users.noreply.github.com>
2025-08-18 15:50:10 -0600

    Haciendo la tarea

--- core/process.php
+++ core/process.php
@@ -4,17 +4,16 @@ $host_aceptados = array('localhost','127.0.0.1');
 $metodo_aceptado = 'POST';
 $usuario_correcto = "Admin";
 $password_correcto = "Admin";
-
-$txt_usuario = $_POST["txt_usuario"];
-$txt_password = $_POST["txt_password"];
+$txt_usuario = ((isset($_POST["txt_usuario"])) ? $_POST["txt_usuario"] : null);
+$txt_password = ((isset($_POST["txt_password"])) ? $_POST["txt_password"] : null);
 $token = "";
 
 if( in_array($_SERVER["HTTP_HOST"],$host_aceptados) ){
-    //la dirección ip es aceptada
-    if($_SERVER["REQUEST_METHOD" == $metodo_aceptado]){
+    //La dirección ip es aceptada
+    if($_SERVER["REQUEST_METHOD"] == $metodo_aceptado){
         //Se acepta el método usado por el usuario
-        if(isset($txt_usuario)&& !empty($txt_usuario)){
-            //Sí se enviaron valores en el campo usuario
+        if(isset($txt_usuario) && !empty($txt_usuario)){
+            //Sí se enviaron valores en el campo usuario.
             if(isset($txt_password) && !empty($txt_password)){
                 //Sí se envió el valor de la contraseña
                 if($txt_usuario==$usuario_correcto){
@@ -24,10 +23,9 @@ if( in_array($_SERVER["HTTP_HOST"],$host_aceptados) ){
                         $ruta = "welcome.php";
                         $msg = "";
                         $codigo_estado = 200;
-                        $texto_estado = "OK";
+                        $texto_estado = "Ok";
                         list($usec,$sec) = explode(' ',microtime());
                         $token = base64_encode(date("Y-m-d H:i:s",$sec).substr($usec,1));
-
                     }else{
                         //El valor ingresado del campo password no es correcto
                         $ruta = "";
@@ -53,34 +51,33 @@ if( in_array($_SERVER["HTTP_HOST"],$host_aceptados) ){
                 $token = "";
             }
         }else{
-            //el campo usuario está vacío
+            //El campo usuario está vacío
             $ruta = "";
-            $msg = "EL CAMPO DE USUARIO ESTÁ VACÍO";
-            $codigo_estado = 401;
-            $texto_estado = "Unauthorized";
-            $token = "";
+                $msg = "EL CAMPO DE USUARIO ESTÁ VACÍO";
+                $codigo_estado = 401;
+                $texto_estado = "Unauthorized";
+                $token = "";
         }
     }else{
-        //el metodo usado por el usuario no es aceptado
+        //El método usado por el usuario no es aceptado
         $ruta = "";
-        $msg = "EL METODO UTILIZADO NO ES EL CORRECTO";
+        $msg = "EL CAMPO DE CONTRASEÑA ESTÁ VACÍO";
         $codigo_estado = 405;
         $texto_estado = "Method Not Allowed";
         $token = "";
     }
 }else{
-//la dirección IP no es aceptada
+    //La dirección IP no es aceptada
     $ruta = "";
     $msg = "SU EQUIPO NO ESTÁ AUTORIZADO PARA REALIZAR ESTA PETICIÓN";
     $codigo_estado = 403;
     $texto_estado = "Forbidden";
     $token = "";
-
 }
 
 $arreglo_respuesta = array(
-    "status" => (   (intval($codigo_estado) == 200)? "OK": "Error" ),
-    "error" => (   (intval($codigo_estado) == 200)? "" : array("code"=>$codigo_estado,"message"=>$msg) ),
+    "status" => (   (intval($codigo_estado) == 200) ? "Ok": "Error" ),
+    "error" => (   (intval($codigo_estado) == 200) ? "" : array("code"=>$codigo_estado,"message"=>$msg) ),
     "data" => array(
         "url"=>$ruta,
         "token"=>$token
@@ -91,6 +88,5 @@ $arreglo_respuesta = array(
 header("HTTP/1.1 ".$codigo_estado." ".$texto_estado);
 header("Content-Type: Application/json");
 echo(json_encode($arreglo_respuesta));
-
 
 ?>
